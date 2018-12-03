929ce8ecde5d2fe24a892f5b531cd242890b74fb
Seung Beum Lee <SB@Seungui-MacBook-Pro.local>
2018-12-03 13:41:44 -0600

    update scripts

--- dependencies.sh
+++ dependencies.sh
@@ -9,4 +9,4 @@ export PYTHONHOME=/study/midus3/environments/conda3
 LD_LIBRARY_PATH=/apps/MCR-R2015b/x86_64_sci7/v90/runtime/glnxa64:/apps/MCR-R2015b/x86_64_sci7/v90/bin/glnxa64:/apps/MCR-R2015b/x86_64_sci7/v90/sys/os/glnxa64
 PATH=/apps/BrainSuite-18a/x86_64_sci7/bin:$PATH
 
-export PS1="(midus3-iamge-processing) $PS1"
+export PS1="(braniac-iamge-processing) $PS1"
