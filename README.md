782c4aeff7707e02d11aeec7f7314c04a2b6f413
DavidSBLee <sleedave90@gmail.com>
2019-01-29 12:36:02 -0600

    update script

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 The repository contains a Python module (braniac.py) for Pre-Processing and Quality-Assessing MRI (T1-weighted, task-based and resting-state fMRI, and DWI) data. 
 
-Usage:
-
-Supply studyname and subject number to "run_braniac.py"
-
-i.e.) python run_braniac.py <study_name> <subject_number>
-
 Necessary Dependencies:
 
 Python 3.7.1 
@@ -17,3 +11,12 @@ FSL 5.0.11
 BrainSuite 18a 
 
 MATLAB R2015b 
+
+Usage:
+
+Supply studyname and subject number to "run_braniac.py"
+
+- source dependancies.sh
+
+- python run_braniac.py <study_name> <subject_number>
+
--- run_braniac.py
+++ run_braniac.py
@@ -25,35 +25,35 @@ subject_number = str(subject_number)
 process_checker = ProcessChecker(study_name, subject_number)
 process_checker.process()
 
-# nifti_converter = NiftiConverter(study_name, subject_number)
-# nifti_converter.process()
+nifti_converter = NiftiConverter(study_name, subject_number)
+nifti_converter.process()
 
-# rotator = NiftiRotator(study_name, subject_number)
-# rotator.process()
+rotator = NiftiRotator(study_name, subject_number)
+rotator.process()
 
-# eprime_converter = ScanEprimeConverter(study_name, subject_number)
-# eprime_converter.process()
+eprime_converter = ScanEprimeConverter(study_name, subject_number)
+eprime_converter.process()
 
-# eprime_divider = ScanEprimeDivider(study_name, subject_number)
-# eprime_divider.process()
+eprime_divider = ScanEprimeDivider(study_name, subject_number)
+eprime_divider.process()
 
-# onset_creator = OnsetCreator(study_name, subject_number)
-# onset_creator.process()
+onset_creator = OnsetCreator(study_name, subject_number)
+onset_creator.process()
 
-# bet_brain_extractor = BetBrainExtractor(study_name, subject_number)
-# bet_brain_extractor.process()
+bet_brain_extractor = BetBrainExtractor(study_name, subject_number)
+bet_brain_extractor.process()
 
-# bse_brain_extractor = BSEBrainExtractor(study_name, subject_number)
-# bse_brain_extractor.process()
+bse_brain_extractor = BSEBrainExtractor(study_name, subject_number)
+bse_brain_extractor.process()
 
-# bias_corrector = BiasCorrector(study_name, subject_number)
-# bias_corrector.process()
+bias_corrector = BiasCorrector(study_name, subject_number)
+bias_corrector.process()
 
-# volume_trimmer = VolumeTrimmer(study_name, subject_number)
-# volume_trimmer.process()
+volume_trimmer = VolumeTrimmer(study_name, subject_number)
+volume_trimmer.process()
 
-# motion_evaluator = MotionEvaluator(study_name, subject_number)
-# motion_evaluator.process()
+motion_evaluator = MotionEvaluator(study_name, subject_number)
+motion_evaluator.process()
 
 print (datetime.now() - startTime)
 
