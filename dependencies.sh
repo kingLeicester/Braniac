b26e5aeacca8d6347c4bb8cab2f6e8c54ad45dc6
DavidSBLee <sleedave90@gmail.com>
2019-06-07 10:14:50 -0500

    edit conda3 path

--- dependencies.sh
+++ dependencies.sh
@@ -2,11 +2,11 @@
 set_fsl_version 5.0.11
 
 # Set conda environment to the one that has neuroprocess functions
-export PATH=/study/midus3/environments/conda3/bin:$PATH
-export PYTHONHOME=/study/midus3/environments/conda3
+export PATH=/home/slee/tools/conda3/bin:$PATH
+export PYTHONHOME=/home/slee/tools/conda3
 
 # Set path to BSE
 LD_LIBRARY_PATH=/apps/MCR-R2015b/x86_64_sci7/v90/runtime/glnxa64:/apps/MCR-R2015b/x86_64_sci7/v90/bin/glnxa64:/apps/MCR-R2015b/x86_64_sci7/v90/sys/os/glnxa64
 PATH=/apps/BrainSuite-18a/x86_64_sci7/bin:$PATH
 
-export PS1="(braniac-iamge-processing) $PS1"
+export PS1="(brainiac-neuroimaging-data-processing) $PS1"
